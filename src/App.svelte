a66c7e7d42434c53ebc73c218a3f1fefe00e8f70
Lulzx <wiserishabh@gmail.com>
2020-10-30 17:36:38 +0530

    input handling

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,83 @@
   import { InlineLoading } from "carbon-components-svelte/src/InlineLoading";
   import { DataTable } from "carbon-components-svelte/src/DataTable";
   import { Grid, Row, Column } from "carbon-components-svelte/src/Grid";
+  import { Form } from "carbon-components-svelte/src/Form";
+  import { Button } from "carbon-components-svelte/src/Button";
+  import { TextArea } from "carbon-components-svelte/src/TextArea";
+  import libgen from 'https://cdn.skypack.dev/libgen';
   let theme = "g10";
+  let query = "";
+  let text = "here we will see the result";
+  let rows = [
+    {
+      id: "a",
+      name: "Book 3",
+      author: "Human",
+      year: 3000,
+      cost: 100,
+      addDate: "2020-10-21",
+    },
+    {
+      id: "b",
+      name: "Book 1",
+      author: "Human",
+      year: 443,
+      cost: 200,
+      addDate: "2020-09-10",
+    },
+    {
+      id: "c",
+      name: "Book 2",
+      author: "Human",
+      year: 80,
+      cost: 150,
+      addDate: "2020-11-24",
+    },
+    {
+      id: "d",
+      name: "Book 6",
+      author: "Human",
+      year: 3000,
+      cost: 250,
+      addDate: "2020-12-01",
+    },
+    {
+      id: "e",
+      name: "Book 4",
+      author: "Human",
+      year: 443,
+      cost: 550,
+      addDate: "2021-03-21",
+    },
+    {
+      id: "f",
+      name: "Book 5",
+      author: "Human",
+      year: 80,
+      cost: 400,
+      addDate: "2020-11-14",
+    },
+  ];
+  const search = async () => {
+    alert(query);
+    const options = {
+      mirror: "http://gen.lib.rus.ec",
+      query: query,
+      count: 20,
+      sort_by: "title",
+      reverse: false,
+    };
+    const data = await libgen.search(options);
+
+    for (let item of data) {
+      const url = `http://libgen.rs/covers/${item.coverurl}`;
+      text += url;
+      console.log(
+        `Title: ${item.title}\nYear: ${item.year}\nAuthor: ${item.author}\nDownload: ${url}`
+      );
+      text += `Title: ${item.title}\nYear: ${item.year}\nAuthor: ${item.author}\nDownload: ${url}`;
+    }
+  };
 </script>
 
 <style lang="scss" global>
@@ -18,7 +94,11 @@
 <Theme persist bind:theme>
   <Header />
   <Content style="background: none; padding: 1rem">
-    <Search />
+    <Form on:submit={search}>
+      <Search bind:value={query} />
+      <Button type="submit">Submit</Button>
+    </Form>
+    <TextArea labelText="App description" placeholder="Enter a description..." bind:value={text}/>
     <Grid>
       <Row>
         <Column style="outline: 0px solid var(--cds-interactive-04)">
@@ -48,7 +128,7 @@
       title="Search Results"
       description="The following are results for your query."
       headers={[{ key: 'name', value: 'Name' }, { key: 'author', value: 'Author' }, { key: 'year', value: 'Year' }, { key: 'cost', value: 'Cost', display: (cost) => cost + ' €' }, { key: 'addDate', value: 'Add date', display: (date) => new Date(date).toLocaleString(), sort: (a, b) => (new Date(a) <= new Date(b) ? -1 : 1) }]}
-      rows={[{ id: 'a', name: 'Book 3', author: 'Human', year: 3000, cost: 100, addDate: '2020-10-21' }, { id: 'b', name: 'Book 1', author: 'Human', year: 443, cost: 200, addDate: '2020-09-10' }, { id: 'c', name: 'Book 2', author: 'Human', year: 80, cost: 150, addDate: '2020-11-24' }, { id: 'd', name: 'Book 6', author: 'Human', year: 3000, cost: 250, addDate: '2020-12-01' }, { id: 'e', name: 'Book 4', author: 'Human', year: 443, cost: 550, addDate: '2021-03-21' }, { id: 'f', name: 'Book 5', author: 'Human', year: 80, cost: 400, addDate: '2020-11-14' }]} />
+      {rows} />
     <PaginationNav total={3} loop />
   </Content>
 </Theme>
