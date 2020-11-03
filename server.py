e551c7c9d60f26c18d0e8a9f5f9cb951d50be825
Lulzx <wiserishabh@gmail.com>
2020-11-03 23:25:14 +0530

    allow * origins

--- server.py
+++ server.py
@@ -109,6 +109,17 @@ s = LibgenSearch()
 
 app = FastAPI()
 
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
