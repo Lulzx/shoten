e575a268111dd253ce3791d6b51d2cc0ff847378
Lulzx <wiserishabh@gmail.com>
2020-11-12 17:33:33 +0530

    add animation while searching for books.
    Co-authored-by: Lulzx <wiserishabh@gmail.com>
    Co-authored-by: Ekamjeet Singh <ekamjeet662@gmail.com>

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -4,7 +4,7 @@
   metatags.description = "Book search engine";
   import { Content } from "carbon-components-svelte";
   import { Search } from "carbon-components-svelte";
-  import { DataTable, DataTableSkeleton } from "carbon-components-svelte";
+  import { DataTable } from "carbon-components-svelte";
   import { Form } from "carbon-components-svelte";
   import { FormGroup } from "carbon-components-svelte";
   import {
@@ -23,7 +23,6 @@
   import Calendar24 from "carbon-icons-svelte/lib/Calendar24";
   import InformationSquare24 from "carbon-icons-svelte/lib/InformationSquare24";
   import { PaginationNav } from "carbon-components-svelte";
-  import { Tag } from "carbon-components-svelte";
 
   let icons = [InformationSquare24, UserProfile24, Network_224, Calendar24];
   const ctx = getContext("Theme");
@@ -87,62 +86,90 @@
   };
 </script>
 
-<Theme persist bind:theme>
-  <Header company="Shoten" platformName="Book Search Engine" href="/">
-    <HeaderUtilities>
-      <!-- <HeaderActionSearch  on:inputSearch={search} /> -->
-      <HeaderGlobalAction
-        aria-label="Settings"
-        icon={theme_icon}
-        on:click={toggle_theme} />
-    </HeaderUtilities>
-  </Header>
-  <Content style="background: none; padding: 1rem">
-    <Form on:submit={search}>
-      <Search
-        bind:value={current_query}
-        placeholder="type book {type}..."
-        autofocus="true" />
-    </Form>
-    <FormGroup legendText="Filter (fields)">
-      <ContentSwitcher selectedIndex={types.indexOf(type)} light="true">
-        {#each types as k}
-          <Switch on:click={() => (type = k)}>
-            <div style="display: flex; align-items: center;">
-              <Icon
-                render={icons[types.indexOf(k)]}
-                style="margin-right: 0.5rem;" />
-              {k}
-            </div>
-          </Switch>
-        {/each}
-      </ContentSwitcher>
-    </FormGroup>
-    {#if state === 'loading'}
-      <DataTableSkeleton {headers} rows={3} />
-    {:else if state === 'onload'}
-      <hr />
-    {:else}
-      <DataTable
-        sortable
-        zebra
-        on:click:row={({ detail }) => {
-          let str = detail.download,
-            hash = str.split('main/')[1],
-            url = window.location.href + 'book?id=' + hash;
-          window.open(url);
-        }}
-        title="Search Results"
-        description="Displaying {shown} out of {total} results for your query."
-        headers={headers.map((x) => ({ key: x, value: x }))}
-        {rows} />
-      <PaginationNav
-        bind:page
-        on:change={search}
-        total={pages}
-        on:click:button--previous={page}
-        on:click:button--next={page}
-        loop="true" />
-    {/if}
-  </Content>
-</Theme>
+<style>
+  :global(.bx--header) {
+    background-color: #161616;
+  }
+  :global(.bx--content-switcher-btn:focus) {
+    box-shadow: inset 0 0 0 2px #ffffff;
+  }
+  :global(.bx--content-switcher--light .bx--content-switcher-btn) {
+    background-color: var(--cds-ui-01, #ffffff);
+  }
+  :global(.bx--content-switcher--light
+      .bx--content-switcher-btn.bx--content-switcher--selected) {
+    color: var(--cds-inverse-01, #ffffff);
+    background-color: var(--cds-ui-05, #161616);
+  }
+  :global(.bx--content-switcher--dark
+      .bx--content-switcher-btn.bx--content-switcher--selected) {
+    color: var(--cds-inverse-01, #000000);
+    background-color: var(--cds-ui-05, #161616);
+  }
+  :global(.bx--header__action) {
+    padding: revert;
+  }
+  :global(.bx--header__action:focus) {
+    border-color: #161616;
+  }
+  :global(.bx--header__action:hover) {
+    background-color: #161616;
+  }
+</style>
+
+{#if ['onload', 'completed'].includes(state)}
+  <Theme persist bind:theme>
+    <Header company="Shoten" platformName="Book Search Engine" href="/">
+      <HeaderUtilities>
+        <HeaderGlobalAction icon={theme_icon} on:click={toggle_theme} />
+      </HeaderUtilities>
+    </Header>
+    <Content style="background: none; padding: 1rem">
+      <Form on:submit={search}>
+        <Search
+          bind:value={current_query}
+          placeholder="type book {type}..."
+          autofocus="true" />
+      </Form>
+      <FormGroup legendText="Filter (fields)">
+        <ContentSwitcher selectedIndex={types.indexOf(type)} light="true">
+          {#each types as k}
+            <Switch on:click={() => (type = k)}>
+              <div style="display: flex; align-items: center;">
+                <Icon
+                  render={icons[types.indexOf(k)]}
+                  style="margin-right: 0.5rem;" />
+                {k}
+              </div>
+            </Switch>
+          {/each}
+        </ContentSwitcher>
+      </FormGroup>
+      <!-- <DataTableSkeleton {headers} rows={3} /> -->
+      {#if state === 'completed'}
+        <DataTable
+          sortable
+          zebra
+          on:click:row={({ detail }) => {
+            let str = detail.download,
+              hash = str.split('main/')[1],
+              url = window.location.href + 'book?id=' + hash;
+            window.open(url);
+          }}
+          title="Search Results"
+          description="Displaying {shown} out of {total} results for your query."
+          headers={headers.map((x) => ({ key: x, value: x }))}
+          {rows} />
+        <PaginationNav
+          bind:page
+          on:change={search}
+          total={pages}
+          on:click:button--previous={page}
+          on:click:button--next={page}
+          loop="true" />
+      {/if}
+    </Content>
+  </Theme>
+{:else if state === 'loading'}
+  <div id="search" />
+{/if}
