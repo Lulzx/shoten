1e6ecb40c067aada6e8bde72d57af785340565ce
Lulzx <wiserishabh@gmail.com>
2020-11-16 05:01:39 +0530

    rename function names

--- backend/main.py
+++ backend/main.py
@@ -71,8 +71,8 @@ class AudiobookInfo(BaseModel):
 def sanitize(row):
     indices = 5, 6
     row = [i for j, i in enumerate(row[:10]) if j not in indices]
-    row = [p.replace("'", "\'").replace('"', '\"') for p in row]
-    size = row[-3].split(' ')
+    row = [p.replace("'", "'").replace('"', '"') for p in row]
+    size = row[-3].split(" ")
     val = size[0]
     ext = size[1]
     if ext == "Kb":
@@ -90,23 +90,34 @@ async def search(query: str = "", search_type: str = "title", page: int = 0):
     search_url = f"http://gen.lib.rus.ec/search.php?{query_string}"
     async with httpx.AsyncClient() as client:
         search_page = await client.get(search_url)
-    soup = bs(search_page.text, 'lxml')
+    soup = bs(search_page.text, "lxml")
     subheadings = soup.find_all("i")
     for subheading in subheadings:
         subheading.decompose()
     try:
-        information_table = soup.find_all('table')[2]
-        count = soup.find_all('font')[2].string.split('|')[0].split(' ')[0]
+        information_table = soup.find_all("table")[2]
+        count = soup.find_all("font")[2].string.split("|")[0].split(" ")[0]
         raw_data = [
             [
-                td.a['href'] if td.find('a') and td.find('a').has_attr("title") and td.find('a')["title"] != ""
-                else ''.join(td.stripped_strings)
+                td.a["href"]
+                if td.find("a")
+                and td.find("a").has_attr("title")
+                and td.find("a")["title"] != ""
+                else "".join(td.stripped_strings)
                 for td in row.find_all("td")
             ]
             for row in information_table.find_all("tr")[1:]
         ]
-        cols = ["id", "author", "title",
-                "publisher", "year", "size", "extension", "download"]
+        cols = [
+            "id",
+            "author",
+            "title",
+            "publisher",
+            "year",
+            "size",
+            "extension",
+            "download",
+        ]
         result = [dict(zip(cols, sanitize(row))) for row in raw_data]
     except:
         count = 0
@@ -141,17 +152,17 @@ async def root():
 
 @app.get("/query/{search_type}/{query}/{page}", response_model=SearchResult)
 @cache(expire=99)
-async def read_item(search_type: str, query: str, page: int):
+async def book_search(search_type: str, query: str, page: int):
     result = await search(query, search_type, page)
-    if not result['results'] and search_type == "title":
+    if not result["results"] and search_type == "title":
         params = GoogleBooks(q=query)
         query_string = urlencode(asdict(params), doseq=True)
         url = f"https://www.googleapis.com/books/v1/volumes?{query_string}"
         async with httpx.AsyncClient() as client:
             response = await client.get(url)
         data = response.json()
-        if data['totalItems'] != 0:
-            query = data['items'][0]['volumeInfo']['title']
+        if data["totalItems"] != 0:
+            query = data["items"][0]["volumeInfo"]["title"]
             result = await search(query, search_type, page)
     return result
 
@@ -160,7 +171,7 @@ async def read_item(search_type: str, query: str, page: int):
 @cache(expire=99)
 async def book_info(code: str):
     start = time.time()
-    regex: str = '<[^<]+?>'
+    regex: str = "<[^<]+?>"
     base_url = "http://library.lol"
     link = base_url + "/main/" + code
     client = httpx.AsyncClient()
@@ -168,10 +179,11 @@ async def book_info(code: str):
     markup = response.text
     soup = bs(markup, "lxml")
     try:
-        image = base_url + soup.find("img")['src']
+        image = base_url + soup.find("img")["src"]
         response = await client.get(image)
-        encoded_image_data = "data:image/png;base64," + \
-                             b64encode(response.content).decode('utf-8')
+        encoded_image_data = "data:image/png;base64," + b64encode(
+            response.content
+        ).decode("utf-8")
         await client.aclose()
     except:
         encoded_image_data = "NO_IMAGE"
@@ -186,45 +198,65 @@ async def book_info(code: str):
         subtitle = heading[1].strip()
     try:
         author_prefix = "Author"
-        author = str(soup.select_one('p:contains({})'.format(
-            author_prefix)))[14:]
-        author = sub(regex, '', author)
+        author = str(soup.select_one("p:contains({})".format(author_prefix)))[14:]
+        author = sub(regex, "", author)
     except:
         author = ""
     try:
-        year = sub(regex, '', str(soup.select_one('p:contains(Publisher)')).split(",")[
-            1].removeprefix(" Year: "))
+        year = sub(
+            regex,
+            "",
+            str(soup.select_one("p:contains(Publisher)"))
+            .split(",")[1]
+            .removeprefix(" Year: "),
+        )
     except IndexError:
         year = ""
     try:
         description_prefix = "Description"
-        description = str(soup.select_one('div:contains({})'.format(
-            description_prefix))).removeprefix("<div>" + description_prefix + ":<br/>").removesuffix("</div>")
+        description = (
+            str(soup.select_one("div:contains({})".format(description_prefix)))
+            .removeprefix("<div>" + description_prefix + ":<br/>")
+            .removesuffix("</div>")
+        )
         description = description.replace("<br />", "")
-        description = sub(regex, '', description)
-        description = ' '.join(description.split())
-        description = description.replace("'", "\'").replace('"', '')
-        description = description.replace('\n', '')
+        description = sub(regex, "", description)
+        description = " ".join(description.split())
+        description = description.replace("'", "'").replace('"', "")
+        description = description.replace("\n", "")
     except:
         description = ""
     end = time.time()
     time_elapsed = end - start
-    result = dict(time_elapsed=time_elapsed, title=title, subtitle=subtitle,
-                  description=description, year=year, author=author,
-                  image=encoded_image_data, direct_url=direct_url)
+    result = dict(
+        time_elapsed=time_elapsed,
+        title=title,
+        subtitle=subtitle,
+        description=description,
+        year=year,
+        author=author,
+        image=encoded_image_data,
+        direct_url=direct_url,
+    )
     return result
 
 
 @app.get("/vox/{query}", response_model=AudiobookInfo)
 @cache(expire=99)
-async def read_item(query: str):
+async def audiobook_search(query: str):
     start = time.time()
     book = Librivox.search_audiobooks(title=query)[0]
-    authors = str(book.authors[0]).split(',')[0]
+    authors = str(book.authors[0]).split(",")[0]
     end = time.time()
     time_elapsed = end - start
-    result = dict(time_elapsed=time_elapsed, title=book.title, description=book.description,
-                  authors=authors, url=book.url, streams=book.streams)
+    result = dict(
+        time_elapsed=time_elapsed,
+        title=book.title,
+        description=book.description,
+        authors=authors,
+        url=book.url,
+        streams=book.streams,
+    )
     return result
 
 
